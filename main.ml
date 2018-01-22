2eebfa50d40c153784737e44f4b2810755eca677
Nicolas Devatine <nico.devatine@gmail.com>
2018-01-22 17:14:07 +0100

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,37 +1,21 @@
 open Tsdl
 open Result
+open Objet
+open Tool
+
 (*eval $(opam config env)*)
 (*ocamlbuild -use-ocamlfind -package tsdl,tsdl_mixer metroidvania.byte*)
 
-let result f msg = match f with
-  | Error (`Msg e) -> Sdl.log "%s: %s" msg e; exit 1
-  | Ok r -> r
-;;
-    
-let create_texture_from_surface r s = match Sdl.create_texture_from_surface r s with
-  | Error (`Msg e) -> Sdl.log "Can't create texture error: %s" e; exit 1
-  | Ok t -> t
-;;
+let o = Objet.create 0 2 "char1.bmp";;
 
-let print_image r i =  match Sdl.load_bmp i with
-  | Error (`Msg e) ->  Sdl.log "Cant load image  error: %s" e; exit 1
-  | Ok s -> match Sdl.render_copy r (create_texture_from_surface r s)  with
-	    | Error (`Msg e) ->  Sdl.log "Can't fill image error: %s" e; exit 1 
-	    | Ok () -> Sdl.render_present r
-;;
-
-let get_window_surface w = match Sdl.get_window_surface w with
-  | Error (`Msg e) -> Sdl.log "Can't get surface error: %s" e; exit 1
-  | Ok s -> s
-;;
-
-let rec wait w = 
+let rec wait r w = Objet.update_pos o (Objet.get_x o) ((Objet.get_y o)+1)  ;
 let event = Sdl.Event.create () in 
-match Sdl.wait_event (Some(event)) with
-  | Error (`Msg e) -> Sdl.log "Could not wait event: %s" e; exit 1
-  | Ok () -> match Sdl.Event.(enum (get event typ )) with
-	    |`Quit -> Sdl.destroy_window w; Sdl.quit ()
-	  | _ -> wait w 
+match Sdl.poll_event (Some(event)) with
+  | false -> Sdl.render_clear r; Tool.display_background "test.bmp" r;Tool.display_object (Objet.get_x o) (Objet.get_y o) (Objet.get_image o) r; Sdl.render_present r; wait r w
+  | true -> match Sdl.Event.(enum (get event typ)) with
+	        |`Quit -> Sdl.destroy_window w; Sdl.quit ()
+            |`Mouse_button_down -> let x = Sdl.Event.get event Sdl.Event.mouse_button_x in let y = Sdl.Event.get event Sdl.Event.mouse_button_y in (Objet.update_pos o x y); wait r w
+            | _ -> Sdl.render_clear r; Tool.display_background "test.bmp" r;Tool.display_object (Objet.get_x o) (Objet.get_y o) (Objet.get_image o) r; Sdl.render_present r; wait r w
 ;;  
   
 let main () = match Sdl.init Sdl.Init.video with
@@ -40,7 +24,7 @@ let main () = match Sdl.init Sdl.Init.video with
 	     | Error (`Msg e) -> Sdl.log "Create window error: %s" e; exit 1
 	     | Ok w -> match Sdl.create_renderer ~flags:Sdl.Renderer.(accelerated + presentvsync) w with
 	 	      | Error (`Msg e) ->  Sdl.log "Can't create renderer error: %s" e; exit 1
-		      | Ok r -> print_image r "test.bmp"; wait w
-;;  
+		      | Ok r -> wait r w
+;; 
 
-let () = main ()
+let () = main ();;
