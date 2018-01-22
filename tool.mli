698503b82d5732c838e837e1550d90f82cbabca5
Nicolas Devatine <nico.devatine@gmail.com>
2018-01-22 17:15:50 +0100

    Create tool.mli

---
+++ tool.mli
@@ -0,0 +1,7 @@
+open Tsdl
+open Result
+
+val display_object : int -> int -> string -> Sdl.renderer -> unit
+val display_background : string -> Sdl.renderer -> unit
+val create_texture_from_surface : Sdl.renderer -> Sdl.surface -> Sdl.texture
+val get_window_surface : Sdl.window -> Sdl.surface
