0c496106d40b1ac12b125f98f9abfca1d995801a
Coupdepoker <35461199+Coupdepoker@users.noreply.github.com>
2018-01-29 17:17:42 +0100

    Update object.mli

--- object.mli
+++ object.mli
@@ -3,8 +3,14 @@ open Result
 
 
 type objet
-val create : int -> int -> string -> objet
+val create : int -> int -> string -> float -> int -> int -> int-> objet
 val get_x : objet -> int
+val get_dx : objet -> int
+val get_dy : objet -> int
 val get_y : objet -> int
-val update_pos : objet -> int -> int -> unit
+val get_vy : objet -> float
+val set_vy : float -> objet -> objet
+val get_vx : objet -> int
+val set_vx : int -> objet -> objet
+val update_pos : objet -> int -> int -> objet
 val get_image : objet -> string
