4c25b88137299ea7d3b1ad82ff62a184981a7c65
Coupdepoker <35461199+Coupdepoker@users.noreply.github.com>
2018-02-05 17:18:02 +0100

    Update tool.ml

--- tool.ml
+++ tool.ml
@@ -1,5 +1,6 @@
 open Tsdl
 open Result
+open Objet
 
 let create_texture_from_surface r s = match (Sdl.create_texture_from_surface r s) with
 | Error (`Msg e)  -> Sdl.log "Can't create texture error : %s" e; exit 1
@@ -11,11 +12,15 @@ let get_window_surface w = match (Sdl.get_window_surface w) with
 | Ok r -> r
 ;;
 
-let display_object x y i r dx dy =  match Sdl.load_bmp i with
-  | Error (`Msg e) ->  Sdl.log "Cant load image  error: %s" e; exit 1
-  | Ok s -> let t = (create_texture_from_surface r s) in let rect = (Sdl.Rect.create x y dx dy) in match Sdl.render_copy ~dst:rect r t  with
-	    | Error (`Msg e) ->  Sdl.log "Can't fill image error: %s" e; exit 1 
-	    | Ok () -> Sdl.destroy_texture t; Sdl.free_surface s
+let rec display_object l r =  match l with 
+						| [] -> ()
+						| o::next -> match Sdl.load_bmp (Objet.get_image o) with
+								  | Error (`Msg e) ->  Sdl.log "Cant load image  error: %s" e; exit 1
+							      | Ok s -> let t = (create_texture_from_surface r s) in 
+											let rect = (Sdl.Rect.create (Objet.get_x o) (Objet.get_y o) (Objet.get_dx o) (Objet.get_dy o)) in 
+											match Sdl.render_copy ~dst:rect r t  with
+											| Error (`Msg e) ->  Sdl.log "Can't fill image error: %s" e; exit 1 
+											| Ok () -> Sdl.destroy_texture t; Sdl.free_surface s; display_object next r
 ;;
 
 let display_background i r =  match Sdl.load_bmp i with
@@ -24,4 +29,3 @@ let display_background i r =  match Sdl.load_bmp i with
 	    | Error (`Msg e) ->  Sdl.log "Can't fill image error: %s" e; exit 1 
 	    | Ok () -> Sdl.destroy_texture t; Sdl.free_surface s
 ;;
-
