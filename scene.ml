3dc4cddb781ec249d7eb81b36a55f4dc89df8099
Coupdepoker <35461199+Coupdepoker@users.noreply.github.com>
2018-02-12 17:12:06 +0100

    Update scene.ml

--- scene.ml
+++ scene.ml
@@ -2,9 +2,11 @@ open Tsdl
 open Result
 open Objet
 
-type scene = { object_list : Objet.objet list ; character_list : Objet.objet list ; background : string } ;;
-let create l1 l2 background = { object_list = l1 ; character_list = l2 ; background = background} ;;
+type scene = { object_list : Objet.objet list ; character_list : Objet.objet list ; background : string ; width : int ; height : int} ;;
+let create l1 l2 background w h = { object_list = l1 ; character_list = l2 ; background = background ; width = w ; height = h } ;;
 let get_characters s = s.character_list ;;
 let get_objects s = s.object_list ;;
 let get_background s = s.background ;;
-let display_scene s r = Tool.display_background s.background r ; Tool.display_object (s.object_list) r ; Tool.display_object (s.character_list) r ;;
+let display_scene s r c = Tool.display_background s.background r c ; Tool.display_object (s.object_list) r c; Tool.display_object (s.character_list) r c;;
+let get_width s = s.width ;;
+let get_height s = s.height ;;
