d1a4e69a7fc2a00f44e8f60c695c39074799a7a0
Coupdepoker <35461199+Coupdepoker@users.noreply.github.com>
2018-02-12 17:12:20 +0100

    Update scene.mli

--- scene.mli
+++ scene.mli
@@ -3,8 +3,10 @@ open Result
 open Objet
 
 type scene
-val create : Objet.objet list -> Objet.objet list -> string -> scene
+val create : Objet.objet list -> Objet.objet list -> string -> int -> int -> scene
 val get_characters : scene -> Objet.objet list
 val get_objects : scene -> Objet.objet list
 val get_background : scene -> string
-val display_scene : scene -> Sdl.renderer -> unit
+val display_scene : scene -> Sdl.renderer -> Sdl.rect -> unit
+val get_width : scene -> int
+val get_height : scene -> int
