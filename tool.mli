3f900327f72fab03aefd7ce7a9f38a7b014f0f49
Coupdepoker <35461199+Coupdepoker@users.noreply.github.com>
2018-02-12 17:11:39 +0100

    Update tool.mli

--- tool.mli
+++ tool.mli
@@ -2,7 +2,7 @@ open Tsdl
 open Result
 open Objet
 
-val display_object : Objet.objet list -> Sdl.renderer -> unit
-val display_background : string -> Sdl.renderer -> unit
+val display_object : Objet.objet list -> Sdl.renderer -> Sdl.rect -> unit
+val display_background : string -> Sdl.renderer -> Sdl.rect -> unit
 val create_texture_from_surface : Sdl.renderer -> Sdl.surface -> Sdl.texture
 val get_window_surface : Sdl.window -> Sdl.surface
