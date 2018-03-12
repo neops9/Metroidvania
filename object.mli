18c2ff0f180607d2398502131846b1917df5ea6b
Nicolas Devatine <nico.devatine@gmail.com>
2018-03-12 16:58:40 +0100

    Update object.mli

--- object.mli
+++ object.mli
@@ -1,16 +1,18 @@
 open Tsdl
 open Result
 
-
 type objet
-val create : int -> int -> string -> float -> int -> int -> int-> objet
+
+val create : int -> int -> Sdl.texture -> float -> int -> int -> int-> int -> objet
 val get_x : objet -> int
+val get_frame : objet -> int
 val get_dx : objet -> int
 val get_dy : objet -> int
 val get_y : objet -> int
 val get_vy : objet -> float
-val set_vy : float -> objet -> objet
+val set_vy : objet -> float -> objet
 val get_vx : objet -> int
-val set_vx : int -> objet -> objet
+val set_vx : objet -> int -> objet
 val update_pos : objet -> int -> int -> objet
-val get_image : objet -> string
+val set_frame : objet -> int -> objet
+val get_texture : objet -> Sdl.texture
