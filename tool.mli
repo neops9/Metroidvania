7341f2d918cd4411ef49b3c9876067c6fdd3fe45
Nicolas Devatine <nico.devatine@gmail.com>
2018-03-12 17:00:07 +0100

    Update tool.mli

--- tool.mli
+++ tool.mli
@@ -3,6 +3,8 @@ open Result
 open Objet
 
 val display_object : Objet.objet list -> Sdl.renderer -> Sdl.rect -> unit
-val display_background : string -> Sdl.renderer -> Sdl.rect -> unit
+val display_background : Sdl.texture -> Sdl.renderer -> Sdl.rect -> unit
+val create_texture_from_image : Sdl.renderer -> string -> Sdl.texture
 val create_texture_from_surface : Sdl.renderer -> Sdl.surface -> Sdl.texture
 val get_window_surface : Sdl.window -> Sdl.surface
+val objet_to_rect : Objet.objet -> Sdl.rect
