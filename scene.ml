746da31b0cbaf4340de6bfc4e02e5abc64da6d3a
Nicolas Devatine <nico.devatine@gmail.com>
2018-03-12 16:59:01 +0100

    Update scene.ml

--- scene.ml
+++ scene.ml
@@ -2,8 +2,25 @@ open Tsdl
 open Result
 open Objet
 
-type scene = { object_list : Objet.objet list ; character_list : Objet.objet list ; background : string ; width : int ; height : int} ;;
-let create l1 l2 background w h = { object_list = l1 ; character_list = l2 ; background = background ; width = w ; height = h } ;;
+type scene = { object_list : Objet.objet list ;
+	       character_list : Objet.objet list ;
+	       background : Sdl.texture ;
+	       width : int ;
+	       height : int } ;;
+
+let create r l1 l2 background w h =
+  let t = match Sdl.load_bmp background with
+    | Error (`Msg e) ->  Sdl.log "Cant load image  error: %s" e; exit 1
+    | Ok s ->
+       match Sdl.create_texture_from_surface r s with
+       | Error (`Msg e) -> Sdl.log "Cant create texture  error: %s" e; exit 1
+       | Ok t -> t
+  in { object_list = l1 ;
+       character_list = l2 ;
+       background = t ;
+       width = w ;
+       height = h } ;;
+
 let get_characters s = s.character_list ;;
 let get_objects s = s.object_list ;;
 let get_background s = s.background ;;
