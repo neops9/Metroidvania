19ae92bebe917faef1156070e710ed03de9892ad
Nicolas Devatine <nico.devatine@gmail.com>
2018-03-12 16:58:12 +0100

    Update object.ml

--- object.ml
+++ object.ml
@@ -1,15 +1,18 @@
 open Tsdl
 open Result
 
-type objet = { x : int; y : int; image : string; vy : float; vx : int; dx :int; dy : int };;
-let create x y image vy vx dx dy = {x = x; y = y; image = image; vy = vy; vx = vx; dx = dx; dy = dy};;
+type objet = { x : int; y : int; vy : float; vx : int; dx :int; dy : int; frame : int; texture : Sdl.texture } ;;
+let create x y texture vy vx dx dy frame = {x; y; vy; vx; dx; dy; frame; texture} ;;
 let get_x o = o.x;;
+let get_frame o = o.frame;;
 let get_y o = o.y;;
 let get_dx o = o.dx;;
 let get_dy o = o.dy;;
-let get_image o = o.image ;;
+let get_texture o = o.texture ;;
 let get_vy o = o.vy ;;
 let get_vx o = o.vx ;;
-let set_vy vy o = create (get_x o) (get_y o) (get_image o) vy (get_vx o) (get_dx o) (get_dy o) ;; 
-let set_vx vx o = create (get_x o) (get_y o) (get_image o) (get_vy o) vx (get_dx o) (get_dy o);; 
-let update_pos o x y = create x y (get_image o) (get_vy o) (get_vx o) (get_dx o) (get_dy o) ;;
+let set_frame o f = { o with frame = f } ;;
+let set_texture o t = { o with texture = t } ;;
+let set_vy o vy = { o with vy = vy } ;;
+let set_vx o vx = { o with vx = vx } ;;
+let update_pos o x y = { o with x = x; y = y } ;;
