ab1b08f86cf095e7eb77238e3bb5c3f41c29c532
Nicolas Devatine <nico.devatine@gmail.com>
2018-03-19 17:14:38 +0100

    Update object.ml

--- object.ml
+++ object.ml
@@ -1,8 +1,8 @@
 open Tsdl
 open Result
 
-type objet = { x : int; y : int; vy : float; vx : int; dx :int; dy : int; frame : int; texture : Sdl.texture } ;;
-let create x y texture vy vx dx dy frame = {x; y; vy; vx; dx; dy; frame; texture} ;;
+type objet = { x : int; y : int; vy : float; vx : int; dx :int; dy : int; frame : int; texture : Sdl.texture; life_time : int } ;;
+let create x y texture vy vx dx dy frame life_time = { x; y; vy; vx; dx; dy; frame; texture; life_time } ;;
 let get_x o = o.x;;
 let get_frame o = o.frame;;
 let get_y o = o.y;;
@@ -16,3 +16,5 @@ let set_texture o t = { o with texture = t } ;;
 let set_vy o vy = { o with vy = vy } ;;
 let set_vx o vx = { o with vx = vx } ;;
 let update_pos o x y = { o with x = x; y = y } ;;
+let get_life_time o = o.life_time ;;
+let set_life_time o lt = { o with life_time = lt } ;;
