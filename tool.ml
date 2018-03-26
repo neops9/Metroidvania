797dd40827d67606ccb0d2d104065a397973ca12
Nicolas Devatine <nico.devatine@gmail.com>
2018-03-26 17:17:32 +0200

    Update tool.ml

--- tool.ml
+++ tool.ml
@@ -1,6 +1,5 @@
 open Tsdl
 open Result
-open Objet
 
 let create_texture_from_surface r s = match (Sdl.create_texture_from_surface r s) with
   | Error (`Msg e)  -> Sdl.log "Can't create texture error : %s" e; exit 1
@@ -17,19 +16,4 @@ let get_window_surface w = match (Sdl.get_window_surface w) with
   | Ok r -> r
 ;;
 
-let rec display_object l r c =  match l with 
-  | [] -> ()
-  | o::next ->
-     let rect = (Sdl.Rect.create ((Objet.get_x o) - (Sdl.Rect.x c))  ((Objet.get_y o) - (Sdl.Rect.y c)) (Objet.get_dx o) (Objet.get_dy o)) in
-     match Sdl.render_copy ~dst:rect r (Objet.get_texture o)  with
-     | Error (`Msg e) ->  Sdl.log "Can't fill image error: %s" e; exit 1 
-     | Ok () -> display_object next r c
-;;
-
-let objet_to_rect o = Sdl.Rect.create (Objet.get_x o) (Objet.get_y o) (Objet.get_dx o) (Objet.get_dy o) ;;
-
-let display_background t r c =
-  match Sdl.render_copy ~src:c r t  with
-  | Error (`Msg e) ->  Sdl.log "Can't fill image error: %s" e; exit 1 
-  | Ok () -> ()
-;;
+let scancode s = Sdl.get_scancode_from_key (Sdl.get_key_from_name s) ;;
