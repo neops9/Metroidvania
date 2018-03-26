a26efc73edd288691a7ec3a22010b0ebbdd1f2a2
Nicolas Devatine <nico.devatine@gmail.com>
2018-03-26 17:16:17 +0200

    Update main.ml

--- main.ml
+++ main.ml
@@ -3,134 +3,74 @@ open Result
 open Objet
 open Tool
 open Scene
+open Bigarray
 
 (*eval $(opam config env)*)
 (*ocamlbuild -use-ocamlfind -package tsdl,tsdl_mixer main.byte*)
 
 (* CTRL X / H / TAB *)
 
-(*
-	Refaire la gestion du clavier avec SDL_GetKeyState (get_keyboard_state) pour plus d'efficacité 
+(* 
+
+   TODO:
+
+   - Animation de saut
+   - Gérer la cadence de tir
+   - Changement de scène
+   - Vie
+   - Adversaire
+   - Menu
+   - animation de tir
+   - living time : trouver une solution pour pas avoir à mettre le time to live des gens
+   qui reste tout le temps à 10000000000
+
 *)
 
 let window_width = 1024;;
 let window_height = 768;;
 
-let camera_width = 640 ;;
-let camera_height = 480 ;;
-
-let c = Sdl.Rect.create 0 0 camera_width camera_height;;
-
-let move_camera c s move_pers =
-  Sdl.Rect.set_x c (((Objet.get_x move_pers) + 50/2) - window_width/2) ;
-  
-  if (Sdl.Rect.x c < 0)
-  then
-    begin
-      let save_y = Sdl.Rect.y c in
-      Sdl.Rect.set_x c 0 ; Sdl.Rect.set_y c (((Objet.get_y move_pers)) - window_height/2); 
-      if Sdl.Rect.y c >= 0
-      then
-	begin
-	  if Sdl.Rect.y c > (Scene.get_height s) - camera_height 
-	  then
-	    Sdl.Rect.set_y c ((Scene.get_height s) - camera_height);
-	end
-      else
-        Sdl.Rect.set_y c save_y ;
-    end
+let keyboard_scene_actions p s r =
+  let keystates = Sdl.get_keyboard_state () in
+  if (keystates.{ Tool.scancode "a" } <> 0) then begin
+    if Objet.is_flip p then 
+      let bullet = Objet.create ((Objet.get_x p) - 10) ((Objet.get_y p) + 50) (Tool.create_texture_from_image r "images/noisette.bmp") 0. (-13) 10 10 0 1000 false false false in
+      { s with object_list = bullet::(s.object_list) }
+    else
+      let bullet = Objet.create ((Objet.get_x p) + 80) ((Objet.get_y p) + 50) (Tool.create_texture_from_image r "images/noisette.bmp") 0. 13 10 10 0 1000 false false false in
+      { s with object_list = bullet::(s.object_list) }
+  end
   else
-    begin
-      if Sdl.Rect.x c > (Scene.get_width s) - camera_width
-      then
-	begin
-	  Sdl.Rect.set_x c ((Scene.get_width s) - camera_width) ;
-	  Sdl.Rect.set_y c (((Objet.get_y move_pers)) - window_height/2) ;
-	  if Sdl.Rect.y c < 0
-	  then
-	    Sdl.Rect.set_y c 0
-	  else
-	    if Sdl.Rect.y c > (Scene.get_height s) - camera_height
-	    then Sdl.Rect.set_y c ((Scene.get_height s) - camera_height)
-	    else ()
-	end	  
-      else
-	begin
-	  Sdl.Rect.set_y c (((Objet.get_y move_pers)) - window_height/2 -50) ;
-	  if Sdl.Rect.y c < 0
-	  then
-	    Sdl.Rect.set_y c 0
-	  else
-	    if Sdl.Rect.y c > (Scene.get_height s) - camera_height then Sdl.Rect.set_y c ((Scene.get_height s) - camera_height)
-	    else ()
-	end
-    end
+    s
 ;;
 
-let rec collision sce o l =
-  match l with
-  | [] -> false
-  | x::s -> if Sdl.has_intersection (objet_to_rect o) (objet_to_rect x) then true else collision sce o s
-;;
-
-let move s obj =
-  let o_x = Objet.update_pos obj (Objet.get_x obj + Objet.get_vx obj) (Objet.get_y obj) in
-  if collision s o_x (Scene.get_objects s) || collision s o_x (Scene.get_characters s)
-  then 
-    let o_y = Objet.update_pos obj (Objet.get_x obj) ((Objet.get_y obj) + int_of_float(Objet.get_vy obj)) in 
-    if collision s o_y (Scene.get_objects s) || collision s o_y (Scene.get_characters s)
-    then
-      Objet.set_vy obj 0.
-    else o_y
+let keyboard_player_actions p = 
+  let keystates = Sdl.get_keyboard_state () in
+  if (keystates.{ Tool.scancode "up" } <> 0) && not (Objet.is_in_air p) then
+    { p with in_air = true; vy = -12. }
+  else if keystates.{ Tool.scancode "left" } <> 0 then
+    { p with vx = -10; frame = (((Objet.get_frame p)+1) mod 25); flip = true }
+  else if keystates.{ Tool.scancode "right" } <> 0 then
+    { p with vx = 10; frame = (((Objet.get_frame p)+1) mod 25); flip = false }
+  else if keystates.{ Tool.scancode "right" } = 0 then
+    Objet.set_vx p 0
+  else if keystates.{ Tool.scancode "left" } = 0 then
+    Objet.set_vx p 0
   else
-    let o_x_y =  Objet.update_pos o_x (Objet.get_x o_x) ((Objet.get_y o_x) + int_of_float(Objet.get_vy o_x)) in 
-    if collision s o_x_y (Scene.get_objects s) || collision s o_x_y (Scene.get_characters s) then Objet.set_vy o_x 0. else o_x_y ;;
-
-let inAir obj s = if Objet.get_y obj = Objet.get_y (move s obj) then false else true ;;
-
-let scene_move s p =
-  if (Objet.get_x p) < 50 then s
-  else
-    {s with object_list = List.map (move s) (List.fold_left (fun acc x -> let o_bis = Objet.set_life_time x ((Objet.get_life_time x) - 10) in if Objet.get_life_time o_bis <= 0 then acc else o_bis::acc) [] (Scene.get_objects s));
-      character_list = List.map (move s) (Scene.get_characters s)} ;;
-
-let keyboard_down e s o = 
-  if e = Sdl.K.space then o
-  else if e = Sdl.K.up then if inAir o s then o else (Objet.set_vy o (-12.))
-  else if e = Sdl.K.right then Objet.set_frame (Objet.set_vx o 10) (((Objet.get_frame o)+1) mod 5)
-  else if e = Sdl.K.left then Objet.set_frame (Objet.set_vx o (-10)) (((Objet.get_frame o)+1) mod 5)
-  else o				
-;;
-
-let keyboard_up e o = 
-  if e = Sdl.K.right then (Objet.set_vx o 0)
-  else if e = Sdl.K.left then (Objet.set_vx o 0)
-  else o
-;;
-
-let scene_keyboard_down p e s r =
-	if e = Sdl.K.space 
-	then 
-	let bullet = Objet.create ((Objet.get_x p) + 80) ((Objet.get_y p) + 50) (Tool.create_texture_from_image r "noisette.bmp") 0. 13 10 10 0 1000 in
-	{ s with object_list = bullet::(s.object_list) }
-	else
-	s
+    p
 ;;
 
 let rec wait p s r w c =
-  let scene = scene_move s p in
-  let pers_temp = Objet.set_vy p ((Objet.get_vy p) +. 0.5) in
-  let pers_temp = Objet.set_texture pers_temp (Tool.create_texture_from_image r ("char"^((string_of_int) (Objet.get_frame pers_temp))^".bmp")) in
-  let move_pers = (move scene pers_temp) in
-  move_camera c scene move_pers;
+  let p = keyboard_player_actions (Movement.move_object s (Objet.update p r)) in
+  let s = keyboard_scene_actions p (Movement.move_scene s p) r in
+  Camera.move c s p;
   let event = Sdl.Event.create () in
   match Sdl.poll_event (Some(event)) with
-  | false -> Sdl.render_clear r; Scene.display_scene scene r c; Tool.display_object [move_pers] r c; Sdl.render_present r; wait move_pers scene r w c
+  | false -> Display.display_game p s r c; 
+    wait p s r w c
   | true -> match Sdl.Event.(enum (get event typ )) with
     |`Quit -> Sdl.destroy_window w; Sdl.quit ()
-    | `Key_down -> let s_bis = scene_keyboard_down move_pers (Sdl.Event.(get event keyboard_keycode)) scene r in let pers = keyboard_down (Sdl.Event.(get event keyboard_keycode)) s_bis move_pers in wait pers s_bis r w c
-    | `Key_up -> let pers = keyboard_up (Sdl.Event.(get event keyboard_keycode)) move_pers in wait pers scene r w c
-    | _ -> Sdl.render_clear r;  Scene.display_scene scene r c; Tool.display_object [move_pers] r c; Sdl.render_present r; wait move_pers scene r w c
+    | _ -> Display.display_game p s r c; 
+      wait p s r w c
 ;;
 
 let main () = match Sdl.init Sdl.Init.video with
@@ -140,9 +80,10 @@ let main () = match Sdl.init Sdl.Init.video with
     | Ok w -> match Sdl.create_renderer ~flags:Sdl.Renderer.(accelerated + presentvsync) w with
       | Error (`Msg e) ->  Sdl.log "Can't create renderer error: %s" e; exit 1
       | Ok r -> Sdl.set_window_resizable w true;
-	 let personnage = Objet.create 10 200 (Tool.create_texture_from_image r "char0.bmp") 0. 0 79 100 0 1000000000 in
-	 let s = Scene.load_scene "scene1" r window_height in
-	 wait personnage s r w c
-;; 
+	let personnage = Objet.create 10 200 (Tool.create_texture_from_image r "images/char0.bmp") 0. 0 79 100 0 1000000000 false true true in
+	let scene = Scene.load_scene "level/scene1" r window_height in
+	let camera = Camera.create (Sdl.Rect.create 0 0 640 480) window_width window_height in
+	wait personnage scene r w camera
+;;
 
 let () = main () ;;
