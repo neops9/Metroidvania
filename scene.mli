5b239c1c087302681cf53fcf70efa899eda924bc
Nicolas Devatine <nico.devatine@gmail.com>
2018-03-27 13:27:24 +0200

    Update scene.mli

--- scene.mli
+++ scene.mli
@@ -2,17 +2,19 @@ open Tsdl
 open Result
 open Objet
 
-type scene = { object_list : Objet.objet list ;
+type scene = { player : objet; 
+		   object_list : Objet.objet list ;
 	       character_list : Objet.objet list ;
 	       texture_list : Sdl.texture list ;
 	       background : Sdl.texture ;
 	       width : int ;
 	       height : int}
   
-val create : Sdl.renderer -> Objet.objet list -> Objet.objet list -> Sdl.texture list -> string -> int -> int -> scene
+val create : Sdl.renderer -> Objet.objet -> Objet.objet list -> Objet.objet list -> Sdl.texture list -> string -> int -> int -> scene
 val get_characters : scene -> Objet.objet list
 val get_objects : scene -> Objet.objet list
 val get_background : scene -> Sdl.texture
 val get_width : scene -> int
 val get_height : scene -> int
-val load_scene : string -> Sdl.renderer -> int -> scene
+val get_player : scene -> objet
+val load_scene : objet -> string -> Sdl.renderer -> int -> scene
