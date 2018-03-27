a737c1704141575c9e91fd499d03fe6a5dafed9b
Nicolas Devatine <nico.devatine@gmail.com>
2018-03-27 13:26:40 +0200

    Update object.ml

--- object.ml
+++ object.ml
@@ -1,8 +1,8 @@
 open Tsdl
 open Result
 
-type objet = { x : int; y : int; vy : float; vx : int; dx :int; dy : int; frame : int; texture : Sdl.texture; life_time : int; in_air : bool; flip : bool; collision : bool; character : bool } ;;
-let create x y texture vy vx dx dy frame life_time in_air collision character = { x; y; vy; vx; dx; dy; frame; texture; life_time; in_air; flip = false; collision; character } ;;
+type objet = { x : int; y : int; vy : float; vx : int; dx :int; dy : int; frame : int; texture : Sdl.texture; life_time : int; in_air : bool; flip : bool; collision : bool; character : bool; bullet_time : int } ;;
+let create x y texture vy vx dx dy frame life_time in_air collision character = { x; y; vy; vx; dx; dy; frame; texture; life_time; in_air; flip = false; collision; character; bullet_time = 0 } ;;
 let get_x o = o.x;;
 let get_frame o = o.frame;;
 let get_y o = o.y;;
@@ -20,8 +20,9 @@ let get_life_time o = o.life_time ;;
 let set_life_time o lt = { o with life_time = lt } ;;
 let is_in_air o = o.in_air ;;
 let set_in_air o b = { o with in_air = b } ;;
-let update o r = { o with vy = o.vy +. 0.5; texture = (Tool.create_texture_from_image r ("images/char"^((string_of_int) (o.frame / 5))^".bmp")) } ;; 
+let update o r = { o with vy = o.vy +. 0.5; texture = (Tool.create_texture_from_image r ("images/char"^((string_of_int) (o.frame / 5))^".bmp")); bullet_time = o.bullet_time - 1 } ;; 
 let objet_to_rect o = Sdl.Rect.create (get_x o) (get_y o) (get_dx o) (get_dy o) ;;
 let is_flip o = o.flip ;;
 let is_collision o = o.collision ;;
 let is_character o = o.character ;;
+let get_bullet_time o = o.bullet_time ;;
