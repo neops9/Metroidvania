663113006b3491d68666566248637144f9963be4
Nicolas Devatine <nico.devatine@gmail.com>
2018-04-03 13:22:02 +0200

    Update movement.ml

--- movement.ml
+++ movement.ml
@@ -12,22 +12,31 @@ let rec collision sce o l =
 let move_object s o =
   let o_x = Objet.update_pos o (Objet.get_x o + Objet.get_vx o) (Objet.get_y o) in
   if collision s o_x (Scene.get_objects s) || collision s o_x (Scene.get_characters s)
-  then 
-    let o_y = Objet.update_pos o (Objet.get_x o) ((Objet.get_y o) + int_of_float(Objet.get_vy o)) in 
-    if collision s o_y (Scene.get_objects s) || collision s o_y (Scene.get_characters s)
-    then
-      if (Objet.get_vy o) >= 0. then
-	Objet.set_vy (Objet.set_in_air o false) 0.
-      else
-	Objet.set_vy o 0.
-    else o_y
+  then
+    if (Objet.get_life_time) o <= 1000 then Objet.set_life_time o 0 else
+      begin
+	let o_y = Objet.update_pos o (Objet.get_x o) ((Objet.get_y o) + int_of_float(Objet.get_vy o)) in 
+	if collision s o_y (Scene.get_objects s) || collision s o_y (Scene.get_characters s)
+	then
+	  if (Objet.get_vy o) >= 0. then
+	    Objet.set_vy (Objet.set_in_air o false) 0.
+	  else
+	    Objet.set_vy o 0.
+	else o_y
+      end
   else
     let o_x_y =  Objet.update_pos o_x (Objet.get_x o_x) ((Objet.get_y o_x) + int_of_float(Objet.get_vy o_x)) in 
-    if collision s o_x_y (Scene.get_objects s) || collision s o_x_y (Scene.get_characters s) then begin 
-      if (Objet.get_vy o_x) >= 0. then
-	Objet.set_vy (Objet.set_in_air o_x false) 0.
+    if collision s o_x_y (Scene.get_objects s) || collision s o_x_y (Scene.get_characters s) then begin
+      if (Objet.get_life_time) o <= 1000 
+      then Objet.set_life_time o 0 
       else
-	Objet.set_vy o_x 0. end else o_x_y 
+	begin
+	  if (Objet.get_vy o_x) >= 0. then
+	    Objet.set_vy (Objet.set_in_air o_x false) 0.
+	  else
+	    Objet.set_vy o_x 0. end
+    end
+    else o_x_y 
 ;;
 
 let move_scene s p =
