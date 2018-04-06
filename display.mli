4438a992435801759b880fe4f7854708c9faee1d
Nicolas Devatine <nico.devatine@gmail.com>
2018-04-06 09:46:18 +0200

    Update display.mli

--- display.mli
+++ display.mli
@@ -5,5 +5,6 @@ open Camera
 
 val display_background : Sdl.texture -> Sdl.renderer -> Sdl.rect -> unit
 val display_object : objet list -> Sdl.renderer -> Sdl.rect -> unit
+val display_personnage : objet -> Sdl.renderer -> Sdl.rect -> unit
 val display_scene : scene -> Sdl.renderer -> Sdl.rect -> unit
 val display_game : objet -> scene -> Sdl.renderer -> camera -> unit
