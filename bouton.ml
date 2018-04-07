9b5fc74ec1bc9fee40ee6702eae4381d10ff85b1
Nicolas Devatine <nico.devatine@gmail.com>
2018-04-07 20:13:51 +0200

    Update bouton.ml

--- bouton.ml
+++ bouton.ml
@@ -1,12 +1,13 @@
 open Tsdl
 
-type bouton = { nom : string; texture : Sdl.texture; x : int; y :int; dx : int; dy : int } ;;
+type bouton = { nom : string; texture : Sdl.texture; x : int; y :int; dx : int; dy : int; est_bouton_courant : bool } ;;
 
-let create nom texture x y dx dy = { nom; texture; x; y; dx; dy } ;;
+let create nom texture x y dx dy est_bouton_courant = { nom; texture; x; y; dx; dy; est_bouton_courant } ;;
 let get_nom b = b.nom ;;
 let get_texture b = b.texture ;;
 let get_x b = b.x ;;
 let get_y b = b.y ;;
 let get_dx b = b.dx ;;
 let get_dy b = b.dy ;;
-
+let est_courant b = b.est_bouton_courant ;;
+let set_courant b v = { b with est_bouton_courant = v } ;;
