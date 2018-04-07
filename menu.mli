a5a6c7fb6a3a700bb580199873a8c71e2216b3d9
Nicolas Devatine <nico.devatine@gmail.com>
2018-04-07 20:16:19 +0200

    Update menu.mli

--- menu.mli
+++ menu.mli
@@ -8,3 +8,5 @@ val get_bouton_list : menu -> bouton list
 val get_bg : menu -> Sdl.texture
 val load_menu : Sdl.renderer -> menu
 val destroy_menu : menu -> unit
+val update_boutons : menu -> menu
+val get_action : menu -> string
